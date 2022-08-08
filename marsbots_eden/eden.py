86eee31e60c490f5cc2e0d858e99b421ea4051ad
Jonathan Miller <jon@jmill.dev>
2022-08-08 10:34:27 +0100

    Updates

--- marsbots_eden/eden.py
+++ marsbots_eden/eden.py
@@ -1,12 +1,61 @@
 import asyncio
+from dataclasses import dataclass, field
 import io
 import json
+from typing import List
 
 import aiohttp
 import discord
 import requests
 
 from marsbots.discord_utils import update_message
+
+
+@dataclass
+class SourceSettings:
+    origin: str
+    author: int
+    author_name: str
+    guild: int
+    guild_name: str
+    channel: int
+    channel_name: str
+
+
+@dataclass
+class EdenClipXSettings:
+    text_input: str
+    image_url: str = ""
+    step_multiplier: float = 1.0
+    color_target_pixel_fraction: float = 0.75
+    color_loss_f: float = 0.0
+    color_rgb_target: tuple[float] = (0.0, 0.0, 0.0)
+    image_weight: float = 0.35
+    n_permuted_prompts_to_add: int = -1
+    width: int = 0
+    height: int = 0
+    num_octaves: int = 3
+    octave_scale: float = 2.0
+    clip_model_options: List = field(
+        default_factory=lambda: [["ViT-B/32", "ViT-B/16", "RN50"]]
+    )
+    num_iterations: tuple[int] = (100, 200, 300)
+
+
+@dataclass
+class StableDiffusionSettings:
+    text_input: str
+    width: int
+    height: int
+    ddim_steps: int
+    plms: bool
+    C: int
+    f: int
+
+
+@dataclass
+class OracleSettings:
+    text_input: str
 
 
 async def generation_loop(
