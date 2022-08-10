65d76de93f01c55a436d84ecfd81121414a426d1
Gene Kogan <kogan.gene@gmail.com>
2022-08-10 14:25:30 +0000

    update schema

--- marsbots_eden/models.py
+++ marsbots_eden/models.py
@@ -17,7 +17,6 @@ class SourceSettings:
 @dataclass
 class EdenClipXConfig:
     text_input: str
-    generator_name: str = "eden-clipx"
     image_url: str = ""
     step_multiplier: float = 1.0
     color_target_pixel_fraction: float = 0.75
@@ -28,23 +27,29 @@ class EdenClipXConfig:
     width: int = 0
     height: int = 0
     num_octaves: int = 3
+    num_iterations: tuple[int] = (100, 200, 300)
     octave_scale: float = 2.0
     clip_model_options: List = field(
         default_factory=lambda: [["ViT-B/32", "ViT-B/16", "RN50"]],
     )
-    num_iterations: tuple[int] = (100, 200, 300)
+    generator_name: str = "eden-clipx"
 
 
 @dataclass
 class StableDiffusionConfig:
+    mode: str
     text_input: str
     width: int = 512
     height: int = 512
-    ddim_steps: int = 166
-    plms: bool = True
+    interpolation_texts: List = field(default_factory=lambda: [])
+    n_interpolate: int = 10
+    ddim_steps: int = 50
+    plms: bool = False
     C: int = 4
     f: int = 8
-    upscale: bool = False
+    scale: float = 12.5
+    seed: int = 13
+    fixed_code: bool = False
     generator_name: str = "stable-diffusion"
 
 
